510465ed5dcd49ce3814ce319bdd6c86cf1909bc
Hector <quimat2@hotmail.com>
2023-03-29 15:36:30 -0600

    primer prototipo de entrega

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 const apiKey = "2f3ded06e1msh5308b724a5d99e7p18c53bjsn3ee67d070dfa";
+const METACRITIC_BASE_URL = 'https://www.metacritic.com'
 
 const searchForm = document.getElementById("search-form");
 const searchInput = document.getElementById("search-input");
@@ -35,6 +36,7 @@ function displayGames(games) {
 
     const gameImg = document.createElement("img");
     gameImg.src = game.thumb;
+    gameImg.alt = game.external;
     gameCard.appendChild(gameImg);
 
     const gameTitle = document.createElement("h3");
@@ -42,31 +44,17 @@ function displayGames(games) {
     gameCard.appendChild(gameTitle);
 
     const gamePrice = document.createElement("p");
-    gamePrice.textContent = `Precio: $${game.price} USD`;
+    gamePrice.textContent = `Precio más bajo: ${game.cheapest} USD`;
     gameCard.appendChild(gamePrice);
-
-    if (game.isOnSale === 1)
-    {
-      const gameDealPrice = document.createElement("p");
-      gameDealPrice.textContent = `Precio en oferta: $${game.salePrice} USD`;
-      gameCard.appendChild(gameDealPrice);
-
-      const gameSavings = document.createElement("p");
-      gameSavings.textContent = `Ahorro: ${game.savings.toFixed(2)}%`;
-      gameCard.appendChild(gameSavings);
-
-      const gameIsOnSale = document.createElement("a");
-      gameIsOnSale.textContent = "Está en rebaja";
-      gameCard.appendChild(gameIsOnSale);
-    }
     
-    if (game.isOnSale === 0)
-    {
-      const gameIsOnSale = document.createElement("a");
-      gameIsOnSale.textContent = "No está en rebaja";
-      gameCard.appendChild(gameIsOnSale);
-    }
-    
+    /*const cheapest = document.createElement("p");
+    cheapest.textContent = `La tienda más barata es: ${game.cheapest.storeName}`;
+    gameCard.appendChild(cheapest); */
+
+    const gameLink = document.createElement("a");
+    gameLink.textContent = "Ver todas las ofertas";
+    gameLink.href = `https://www.cheapshark.com/redirect?dealID=${game.dealID}`;
+    gameCard.appendChild(gameLink);
 
     gameGrid.appendChild(gameCard);
   });
